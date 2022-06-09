6b7d7e27f77622e514b1b3e068c0645f7655db56
iago ferreira <iagoantunes.f@hotmail.com>
2022-06-09 17:20:13 -0300

    Create Category

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -22,5 +22,15 @@ namespace BookStoreWeb.Controllers
         {
             return View();
         }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
